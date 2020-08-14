7053aa3b4fbe1c6bfb0bb151216bac39664a720b
Mark F <the.mark.fullton@gmail.com>
2020-08-14 08:18:28 -0700

    Added url and description to test cases; styled accordingly

--- routes/twiceRemovedController.js
+++ routes/twiceRemovedController.js
@@ -30,13 +30,21 @@ module.exports = (app) => {
     var testItem = [
       {
         headline: "Test Headline",
+        url:
+          "https://www.wired.com/story/want-free-coding-lessons-twitch-real-time/",
         imageURL:
           "https://i.pinimg.com/originals/6e/5f/0d/6e5f0db3986c094e3b4c22d12b01e764.jpg",
+        description:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer diam dui, elementum eu auctor id, pharetra rutrum justo. Aliquam dapibus tincidunt vulputate. Nullam a mi mollis, pharetra nisi dignissim, elementum mi. Integer ullamcorper metus et neque porta, at iaculis nisi aliquam.",
       },
       {
         headline: "Second Test",
+        url:
+          "https://www.wired.com/story/python-language-more-popular-than-ever/",
         imageURL:
           "https://r1.ilikewallpaper.net/iphone-wallpapers/download/99644/kratos-as-cyberpunk-iphone-wallpaper-ilikewallpaper_com_200.jpg",
+        description:
+          "https://www.wired.com/story/python-language-more-popular-than-ever/",
       },
     ];
 
