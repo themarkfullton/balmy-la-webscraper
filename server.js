f38530248a1cf2bcc0a257a5f2cc8440a3899dda
Mark F <the.mark.fullton@gmail.com>
2020-08-17 16:37:31 -0700

    Switched position of route and server listen

--- server.js
+++ server.js
@@ -2,14 +2,13 @@ const express = require("express");
 const exphbs = require("express-handlebars");
 const session = require("express-session");
 var db = require("./models");
-require("dotenv").config();
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
-app.use(express.static(__dirname + "/public/"));
+app.use(express.static(__dirname + "/public"));
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 
 app.engine(
   "handlebars",
@@ -20,8 +19,8 @@ app.engine(
 
 app.set("view engine", "handlebars");
 
+var routes = require("./routes/balmyLAController")(app);
+
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
 });
-
-var routes = require("./routes/balmyLAController")(app);
