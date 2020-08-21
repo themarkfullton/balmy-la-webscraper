bf5da8adcd6ad19b3d04566bc2ef8f1a6a4aeac7
Mark F <the.mark.fullton@gmail.com>
2020-08-20 20:41:59 -0700

    Changed cheerio to temp ng-scope

--- routes/balmyLAController.js
+++ routes/balmyLAController.js
@@ -68,7 +68,7 @@ module.exports = (app) => {
           weatherToSend.data.push({
             dayName: $(element).find(".day").text(),
             dayNumber: $(element).find(".date").text(),
-            temp: $(element).find(".Temperature").text(),
+            temp: $(element).find(".temp").children(".ng-scope").text(),
             weather: $(element).find("img").attr("src"),
             weatherDesc: $(element).find(".description").text(),
           });
