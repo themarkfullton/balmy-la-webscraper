2fab3bcfacd2e30bb5ce7981aa0b67b94a49726c
Mark F <the.mark.fullton@gmail.com>
2020-08-20 20:52:25 -0700

    Changed temp to two finds in a row

--- routes/balmyLAController.js
+++ routes/balmyLAController.js
@@ -68,7 +68,7 @@ module.exports = (app) => {
           weatherToSend.data.push({
             dayName: $(element).find(".day").text(),
             dayNumber: $(element).find(".date").text(),
-            temp: $(element).find(".temp").children(".ng-scope").text(),
+            temp: $(element).find(".temp").find("span").text(),
             weather: $(element).find("img").attr("src"),
             weatherDesc: $(element).find(".description").text(),
           });
