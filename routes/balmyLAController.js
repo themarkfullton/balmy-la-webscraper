b8c19c52b047b52ce8cec063de7f62f7357c3df3
Mark F <the.mark.fullton@gmail.com>
2020-08-20 18:55:57 -0700

    Removed await from axios call

--- routes/balmyLAController.js
+++ routes/balmyLAController.js
@@ -82,7 +82,7 @@ module.exports = (app) => {
           user: cookieUser,
         });*/
       })
-      .catch((err) => res.send(`Axios failed: ${err.message}`));
+      .catch((err) => res.send(`Axios failed: ${err}`));
 
     /*const weatherToSend = {
       data: [
